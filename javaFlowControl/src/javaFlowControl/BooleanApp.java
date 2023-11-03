a0c3e2b9fb4abb6b4d0d80c91ba59b9051993cd4
DongukHam <hdw8607@gmail.com>
2023-11-03 12:52:22 +0900

    update contents

---
+++ javaFlowControl/src/javaFlowControl/BooleanApp.java
@@ -0,0 +1,22 @@
+package javaFlowControl;
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello world";
+		//String true = "Hello world";  --> error occur / reserved word
+		
+		System.out.println(foo.contains("world"));
+		System.out.println(foo.contains("ham"));
+		
+	}
+
+}
---
+++ javaFlowControl/src/javaFlowControl/ComparisonOperatorApp.java
@@ -0,0 +1,16 @@
+package javaFlowControl;
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(1 > 1); // false
+		System.out.println(1 == 1); // true
+		System.out.println(1 < 1); // false
+		System.out.println(1 >= 1); // true
+		
+
+	}
+
+}
